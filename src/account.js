11f0f8039c4d5a3008c4476f7d1c87500515b475
will <denswilliam@hotmail.com>
2017-11-06 01:37:10 +0100

    Implement getDelegate and getPublicKey

--- src/account.js
+++ src/account.js
@@ -2,6 +2,21 @@ import axios from 'axios'
 import { getEndpoint } from './api'
 
 /**
+ * Get public key from address
+ * @param {string} - Address to get public key
+ * @return {Promise<Response>} Public key
+ */
+export const getPublicKey = (address) => {
+  return axios.get(`${getEndpoint()}/api/accounts/getPublicKey?address=${address}`)
+  .then((res) => {
+    return res.data.publicKey
+  })
+  .catch((err) => {
+    if (err) console.log(err)
+  })
+}
+
+/**
  * Get balance from address
  * @param {string} - Address to get balance
  * @return {Promise<Response>} Balance
@@ -40,6 +55,21 @@ export const getTransactions = (address) => {
   return axios.get(`${getEndpoint()}/api/transactions?recipientId=${address}&senderId=${address}`)
   .then((res) => {
     return res.data.transactions
+  })
+  .catch((err) => {
+    if (err) console.log(err)
+  })
+}
+
+/**
+ * Get delegates list from address
+ * @param {string} - Address to get vote delegates
+ * @return {Promise<Response>} Array of delegates
+ */
+export const getDelegates = (address) => {
+  return axios.get(`${getEndpoint()}/api/accounts/delegates?address=${address}`)
+  .then((res) => {
+    return res.data.delegates
   })
   .catch((err) => {
     if (err) console.log(err)
--- test/AccountSpec.js
+++ test/AccountSpec.js
@@ -10,7 +10,7 @@ describe('account', () => {
       return account.getBalance(address)
         .then((res) => {
           expect(res).to.be.a('number')
-          expect(res).to.be.equal(20)
+          expect(res).to.be.equal(19)
         })
         .catch((e) => {
           console.log(e)
@@ -36,7 +36,34 @@ describe('account', () => {
     it('should return transactions from address', () => {
       return account.getTransactions(address)
         .then((res) => {
-          expect(res.length).to.be.equal(2)
+          expect(res.length).to.be.equal(3)
+        })
+        .catch((e) => {
+          console.log(e)
+          throw e
+        })
+    })
+  })
+
+  describe('account.getDelegates()', () => {
+    it('should return delegates from address', () => {
+      return account.getDelegates(address)
+        .then((res) => {
+          expect(res.length).to.be.equal(1)
+          expect(res[0].username).to.be.equal('darkjarunik')
+        })
+        .catch((e) => {
+          console.log(e)
+          throw e
+        })
+    })
+  })
+
+  describe('account.getPublicKey()', () => {
+    it('should return public key from address', () => {
+      return account.getPublicKey(address)
+        .then((res) => {
+          expect(res).to.be.equal('033e4ea71cf6040e0cb6e4cf1a762a87b10d5f696975a657272d5703517a8c90f6')
         })
         .catch((e) => {
           console.log(e)
