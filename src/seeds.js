46d057e7def55d81482cbfe85a0f3bb68983e518
William Dens <denswilliam@hotmail.com>
2017-11-26 21:53:40 +0100

    Add seeds

--- src/seeds.js
+++ src/seeds.js
@@ -1,6 +1,8 @@
 const seeds = {
   MAIN: [
-    'https://node1.arknet.cloud'
+    'https://node1.arknet.cloud',
+    'https://api.arknode.net',
+    'https://api.arkcoin.net'
   ],
   DEV: [
     'https://dev.arkcoin.net'
